b1871ee778db23c85903a09608972170b1651b48
nikhil7476 <nikhil9027917476@gmail.com>
2025-07-25 22:57:47 +0530

    new file:   src/pages/tools/[id].js 	new file:   src/pages/tools/index.js 	new file:   src/utils/toolsData.js

---
+++ src/pages/tools/[id].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ToolDetail() {
+  return <div>ToolDetail</div>;
+}
+
+export default ToolDetail;
---
+++ src/utils/toolsData.js
@@ -0,0 +1,10 @@
+toolsData = [
+  {
+    id: 1,
+    title: "Visual Studio Code",
+    description:
+      "A powerful and popular code editor with a rich ecosystem of extensions, perfect for web development.",
+    image: "/vscode-logo.png",
+    link: "https://nikhil-portfolio.com/tools/vscode",
+  },
+];
