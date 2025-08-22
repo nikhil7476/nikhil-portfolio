d7965b10570d18ed5cba128b82fea901e23f5a6b
nikhil7476 <nikhil9027917476@gmail.com>
2025-08-22 20:34:08 +0530

    modified:   src/pages/_document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <meta name="author" content="Nikhil Mishra" />
         <meta name="publisher" content="Nikhil Mishra" />
         <meta name="robots" content="index, follow" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/nikhil-logo.jpeg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap"
           rel="stylesheet"
