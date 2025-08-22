369caecafb8df242e834e2f5769e1a729b2054be
nikhil7476 <nikhil9027917476@gmail.com>
2025-08-22 21:24:37 +0530

    new file:   public/website-assets/faqs.png 	modified:   src/pages/contact-me.js 	modified:   src/styles/globals.css

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -4,6 +4,10 @@ import Banner from "@/components/Banner";
 import bannerData from "@/utils/bannerData";
 import { Col, Container, Row } from "react-bootstrap";
 import ContactForm from "@/components/ContactForm";
+import accordionData from "@/utils/accordionData";
+import FaqAccordion from "@/components/FaqAccordion";
+import Image from "next/image";
+import Link from "next/link";
 
 function Contact() {
   return (
@@ -13,9 +17,72 @@ function Contact() {
       <section>
         <Container>
           <Row>
-            <Col xl={6} lg={6} md={6} sm={12} xs={12}></Col>
+            <Col xl={6} lg={6} md={6} sm={12} xs={12}>
+              <p>
+                Whether {"you’re"} looking for a new website, a custom software
+                solution, or digital marketing services, {"we’re"} here to help.
+                Fill out the form below or reach out directly, and {"we’ll"} get
+                back to you as soon as possible.
+              </p>
+              <div>
+                <div></div>
+                <div>
+                  <h3>Mail Me</h3>
+                  <Link href="mailto:nikhilmishra7476@gmail.com">
+                    nikhilmishra7476@gmail.com
+                  </Link>
+                </div>
+              </div>
+              <div>
+                <div></div>
+                <div>
+                  <h3>Call Me</h3>
+                  <Link href="mailto:nikhilmishra7476@gmail.com">
+                    nikhilmishra7476@gmail.com
+                  </Link>
+                </div>
+              </div>
+              <div>
+                <div></div>
+                <div>
+                  <h3>Reach Me</h3>
+                  <Link href="mailto:nikhilmishra7476@gmail.com">
+                    nikhilmishra7476@gmail.com
+                  </Link>
+                </div>
+              </div>
+            </Col>
             <Col xl={6} lg={6} md={6} sm={12} xs={12}>
               <ContactForm />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col xl={6} lg={6} md={6} sm={12} xs={12}>
+              <Image
+                src="/website-assets/faqs.png"
+                alt="FAQs-Image"
+                title="FAQs"
+                width={350}
+                height={350}
+                style={{ width: "100%", height: "auto" }}
+              />
+            </Col>
+            <Col
+              xl={6}
+              lg={6}
+              md={6}
+              sm={12}
+              xs={12}
+              className="align-content-center"
+            >
+              <span>FAQs</span>
+              <h2>Frequently Asked Questions</h2>
+              <hr />
+              <FaqAccordion data={accordionData.about} />
             </Col>
           </Row>
         </Container>
