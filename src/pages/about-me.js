4154aab930f91320c7c853930556186102ffdfbc
nikhil7476 <nikhil9027917476@gmail.com>
2025-08-30 00:50:33 +0530

    modified:   src/pages/about-me.js

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -185,7 +185,7 @@ function About() {
                 key={tech.id}
                 xl={3}
                 lg={3}
-                md={6}
+                md={4}
                 sm={6}
                 xs={6}
                 className="mb-3 text-center align-content-center"
