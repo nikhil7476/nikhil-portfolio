174c790d5ea140c0bd36ac1becb6a93584ffcc6f
nikhil7476 <nikhil9027917476@gmail.com>
2025-09-05 22:01:44 +0530

    modified:   src/components/FaqAccordion.js

--- src/components/FaqAccordion.js
+++ src/components/FaqAccordion.js
@@ -6,60 +6,58 @@ import { useState } from "react";
 import Image from "next/image";
 
 const FaqAccordion = ({ data }) => {
-  const [activeKey, setActiveKey] = useState(null);
+  const [activeKey, setActiveKey] = useState("0");
 
   const handleToggle = (key) => {
     setActiveKey(activeKey === key ? null : key);
   };
 
   return (
-    <>
-      <Container>
-        <Row>
-          <Col xl={5} lg={5} md={6} sm={12} xs={12} className="mb-3" >
-            <Image
-              src="/website-assets/faqs.png"
-              alt="FAQs-Image"
-              title="FAQs"
-              width={350}
-              height={350}
-              style={{ width: "100%", height: "auto" }}
-            />
-          </Col>
-          <Col
-            xl={7}
-            lg={7}
-            md={6}
-            sm={12}
-            xs={12}
-            className="mb-3 align-content-center"
-          >
-            <span>FAQs</span>
-            <h2>Frequently Asked Questions</h2>
-            <hr />
-            <Accordion activeKey={activeKey} flush>
-              {data.map((item, index) => (
-                <Accordion.Item
-                  eventKey={index}
-                  key={item.id || index}
-                  className="mb-3"
-                >
-                  <Accordion.Header onClick={() => handleToggle(index)}>
-                    {activeKey === index ? (
-                      <FaMinus className="mx-2" />
-                    ) : (
-                      <FaPlus className="mx-2" />
-                    )}{" "}
-                    {item.title}
-                  </Accordion.Header>
-                  <Accordion.Body>{item.content}</Accordion.Body>
-                </Accordion.Item>
-              ))}
-            </Accordion>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container>
+      <Row>
+        <Col xl={5} lg={5} md={6} sm={12} xs={12} className="mb-3">
+          <Image
+            src="/website-assets/faqs.png"
+            alt="FAQs-Image"
+            title="FAQs"
+            width={350}
+            height={350}
+            style={{ width: "100%", height: "auto" }}
+          />
+        </Col>
+        <Col
+          xl={7}
+          lg={7}
+          md={6}
+          sm={12}
+          xs={12}
+          className="mb-3 align-content-center"
+        >
+          <span>FAQs</span>
+          <h2>Frequently Asked Questions</h2>
+          <hr />
+          <Accordion activeKey={activeKey} flush>
+            {data.map((item, idx) => (
+              <Accordion.Item
+                eventKey={String(idx)}
+                key={item.id || idx}
+                className="mb-3"
+              >
+                <Accordion.Header onClick={() => handleToggle(String(idx))}>
+                  {activeKey === String(idx) ? (
+                    <FaMinus className="mx-2" />
+                  ) : (
+                    <FaPlus className="mx-2" />
+                  )}
+                  {item.title}
+                </Accordion.Header>
+                <Accordion.Body>{item.content}</Accordion.Body>
+              </Accordion.Item>
+            ))}
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
