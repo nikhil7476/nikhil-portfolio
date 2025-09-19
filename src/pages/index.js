2191d3eb638d4e07e1473135233617975b965e47
nikhil7476 <nikhil9027917476@gmail.com>
2025-09-19 22:48:40 +0530

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ function Home() {
                     key={index}
                     className="d-flex align-items-start gap-3 mb-3"
                   >
-                    <div>{item.icon && <item.icon className="fs-6" />}</div>
+                    <div className="fs-6">{item.icon}</div>
                     <div>
                       <h3>{item.title}</h3>
                       <p>{item.description}</p>
